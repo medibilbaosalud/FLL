21cbe4e5e1842994d3adf4a156840e98b64b87a2
medibilbaosalud <medibilbaosalud@gmail.com>
2025-10-24 21:28:00 +0200

    fix: expose geojson modules to typescript

--- global.d.ts
+++ global.d.ts
@@ -1,1 +1,17 @@
-/// <reference path="./types/modules/index.d.ts" />
+import type { FeatureCollection } from "geojson";
+import type { SiteTimeSeries } from "./lib/types";
+
+declare module "@/data/sites.geojson" {
+  const value: FeatureCollection;
+  export default value;
+}
+
+declare module "@/data/time_series.json" {
+  const value: SiteTimeSeries[];
+  export default value;
+}
+
+declare module "*.geojson" {
+  const value: FeatureCollection;
+  export default value;
+}
